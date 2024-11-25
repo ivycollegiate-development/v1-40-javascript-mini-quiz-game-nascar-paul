f3d47fced82ddf3daef46f6a752c86286eef7fb6
Paul Jones <paul.tyler.jones@gmail.com>
2024-11-25 07:53:40 +0000

    Adds java programming

---
+++ java.js
@@ -0,0 +1,21 @@
+// Variables (explain what each variable is for):
+const correctAnswer = “Paris”;
+let score = 0;
+
+const buttons = document.querySelectorAll(".answerButton");
+const feedback = document.getElementById("feedback");
+const scoreDisplay = document.getElementById("score");
+
+// Event Listeners (explain how they work):
+
+buttons.forEach(button => {
+    button.addEventListener("click", function() {
+        if (button.innerHTML === correctAnswer) {
+            feedback.innerHTML = "Correct!";
+            score++;
+        } else {
+            feedback.innerHTML = "Try Again!";
+        }
+        scoreDisplay.innerHTML = `Score: ${score}`;
+    });
+});
